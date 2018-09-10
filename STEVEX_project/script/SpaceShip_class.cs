a7b142d496e3f446ef960b789a834b8bbc4c053b
MrSTEVETH <43130097+MrsteveTH@users.noreply.github.com>
2018-09-10 14:47:20 +0700

    SpaceShip_classV0.1
    Added Inc and Dc function

--- STEVEX_project/script/SpaceShip_class.cs
+++ STEVEX_project/script/SpaceShip_class.cs
@@ -7,18 +7,20 @@ public class SpaceShip
     private String name;
     private Color color;
  	
-
-    
-    
     public float getSpeed (){return speed;}
     public void setSpeed (float s){speed = s;}
     public String getName (){return speed;}
     public void setName (String n){name = n;}
+    public Color getColor (){return color;}
+    public void setColor (Color c){name = n;}
 
-    private void OfficeSort (int a, int b)
-    {
-        int answer;
-        answer = a + b;
-        Debug.Log("Office Supplies total: " + answer);
-    }
+  	public downSpeed(float x){
+  		speed -= x;
+  	}
+
+  	public increaseSpeed(float x){
+  		speed += x;
+  	}
+
+  	
 }
